def066351935b89ae28279dd9e9d47100e90fdfd
BYEDUCK <32582309+BYEDUCK@users.noreply.github.com>
2017-11-16 21:48:46 +0100

    Multitasking improved
    Przeniesienie przetrzymywanego numeru portu z serwera na wątek klienta.

---
+++ WORKINGone/FileMetaData.java
@@ -0,0 +1,14 @@
+public class FileMetaData {
+    private int fileLength;
+    private String filePath;
+    public int getFileLength(){
+        return this.fileLength;
+    }
+    public String getFilePath() {
+        return filePath;
+    }
+    public FileMetaData(String filePath, int fileLength){
+        this.fileLength = fileLength;
+        this.filePath = filePath;
+    }
+}
